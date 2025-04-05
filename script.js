6c7555f4ddc0c5c9d11f8b24ab53474f470f9082
Robert Coluccio <65334150+robertjc83@users.noreply.github.com>
2025-04-05 10:48:10 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+VANTA.GLOBE({
+  el: "#vanta-bg",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  color: 0x00ffff, // tweak this for color style
+  backgroundColor: 0x000000
+});
 
