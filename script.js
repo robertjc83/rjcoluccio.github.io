593224effd8a513311c546acf44d5ad95cd2aeee
Robert Coluccio <65334150+robertjc83@users.noreply.github.com>
2025-04-05 10:56:17 -0400

    Update script.js
    changed color

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ VANTA.GLOBE({
   minWidth: 200.00,
   scale: 1.00,
   scaleMobile: 1.00,
-  color: 0xff6f00, // tweak this for color style
-  backgroundColor: 0x000000
+  color: 0x4f900, // tweak this for color style
+  backgroundColor: 0xa0a10
 });
 
